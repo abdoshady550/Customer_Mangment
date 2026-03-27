c818b0b9e0aa0e9c412e1f7742b7e24618f56380
Abdelrahman Mahmoud Shady <abdoshady550@gmail.com>
2026-03-27 14:49:39 +0200

    Edit HoastApp to use Image to containrize API project replace local process

--- Customer_Mangment.AppHost/AppHost.cs
+++ Customer_Mangment.AppHost/AppHost.cs
@@ -14,10 +14,18 @@ var rabbit = builder.AddRabbitMQ("rabbitmq",
     .WithDataVolume("rabbitmq-data")
     .WithManagementPlugin(port: 15672);
 
-builder.AddProject<Projects.Customer_Mangment>("customer-mangment")
+//API
+builder.AddContainer("customer-mangment", "customer-mangment")
+//builder.AddProject<Projects.Customer_Mangment>("customer-mangment")
     .WithReference(sqlDb)
     .WithReference(mongoDb)
     .WithReference(rabbit)
-    .WaitFor(rabbit);
+    .WaitFor(rabbit)
+    .WithHttpEndpoint(port: 5000, targetPort: 8080)
+    .WithUrls(c =>
+    {
+        c.Urls.Add(new() { Url = "http://localhost:5000/swagger", DisplayText = "Swagger UI" });
+        c.Urls.Add(new() { Url = "http://localhost:5000/scalar/v1", DisplayText = "Scalar UI" });
+    });
 
 builder.Build().Run();
