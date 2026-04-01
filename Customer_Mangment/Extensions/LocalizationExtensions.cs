1e9226c95a00534769451ca638831f612882a819
Abdelrahman Mahmoud Shady <abdoshady550@gmail.com>
2026-04-01 11:08:43 +0200

    Add Localization Options for FallBack Culture

--- Customer_Mangment/Extensions/LocalizationExtensions.cs
+++ Customer_Mangment/Extensions/LocalizationExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace Customer_Mangment.Extensions
+﻿using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
+namespace Customer_Mangment.Extensions
 {
     public static class LocalizationExtensions
     {
@@ -6,6 +9,27 @@
         {
             services.AddLocalization(options =>
                 options.ResourcesPath = "");
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var defaultCulture = new CultureInfo("en");
+
+                var supported = new[]
+                {
+                    new CultureInfo("en"),
+                    new CultureInfo("en-US"),
+                    new CultureInfo("ar"),
+                    new CultureInfo("ar-EG"),
+                };
+
+                options.DefaultRequestCulture = new RequestCulture(defaultCulture);
+                options.SupportedCultures = supported;
+                options.SupportedUICultures = supported;
+
+
+                options.FallBackToParentCultures = true;
+                options.FallBackToParentUICultures = true;
+            });
 
             return services;
         }
